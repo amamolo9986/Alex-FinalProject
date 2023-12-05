a1cf0c465cc09a04c6fa941c1cf2c9952d79bce5
amamolo9986 <amamolo9986@yahoo.com>
2023-12-05 16:54:09 -0600

    Initial commit

---
+++ src/main/java/com/hotnslicy/finalproject/AlexFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hotnslicy.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotnslicy/finalproject/AlexFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotnslicy.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
